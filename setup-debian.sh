d864be5a8542f70c6bd79cd818526a9c6a58d4a0
Thomas Preston <thomas.preston@codethink.co.uk>
2019-02-07 15:55:08 +0000

    setup-debian: Use functions

--- setup-debian.sh
+++ setup-debian.sh
@@ -4,52 +4,89 @@ EMAIL="thomas.preston@codethink.co.uk"
 DIR_GNUPG="~/.gnupg"
 DIR_SSH="~/.ssh"
 
-if ! ls "$DIR_GNUPG" "$DIR_SSH" &>/dev/null; then
-	if ! whiptail --yesno "No GPG or SSH keys found. Continue?" 7 60; then
-		exit 1
+check_keys()
+{
+	if ! ls "$DIR_GNUPG" "$DIR_SSH" &>/dev/null; then
+		if ! whiptail --yesno "No GPG or SSH keys found. Continue?" \
+				7 60; then
+			exit 1
+		fi
 	fi
-fi
+}
 
-sudo apt install \
-	build-essential \
-	exim4 \
-	firmware-iwlwifi \
-	fonts-nanum \
-	fonts-symbola \
-	git \
-	gnupg2 \
-	hexchat \
-	moreutils \
-	openssh-server \
-	pass \
-	thunderbird \
-	tmux \
-	vim
+apt_install()
+{
+	sudo apt install \
+		build-essential \
+		exim4 \
+		firmware-iwlwifi \
+		fonts-nanum \
+		fonts-symbola \
+		git \
+		gnupg2 \
+		hexchat \
+		moreutils \
+		openssh-server \
+		pass \
+		thunderbird \
+		tmux \
+		vim
+}
 
-if whiptail --yesno "Would you like to generate a new GPG key?" 7 60; then
-	gpg2 --full-gen-key
-fi
-if whiptail --yesno "Would you like to generate a new SSH key?" 7 60; then
-	ssh-keygen -t rsa -b 4096 -C $EMAIL
-fi
+gen_keys()
+{
+	if whiptail --yesno "Would you like to generate a new GPG key?" 7 60
+	then
+		gpg2 --full-gen-key
+	fi
+	if whiptail --yesno "Would you like to generate a new SSH key?" 7 60
+	then
+		ssh-keygen -t rsa -b 4096 -C $EMAIL
+	fi
+}
 
-git config --global user.email "thomas.preston@codethink.co.uk"
-git config --global user.name "Thomas Preston"
+setup_git()
+{
+	git config --global user.email "thomas.preston@codethink.co.uk"
+	git config --global user.name "Thomas Preston"
+}
 
-git clone git@gitlab.com:tpreston/journal.git
+get_journal()
+{
+	git clone git@gitlab.com:tpreston/journal.git
+}
 
-(cd Documents/
-	git clone https://github.com/tompreston/notes;
-	git clone https://github.com/tompreston/dotfiles;
-	(cd dotfiles; make install))
+get_notes()
+{
+	git clone https://github.com/tompreston/notes Documents/notes2
+}
 
-# vim pathogen and plugins
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+get_dotfiles()
+{
+	git clone https://github.com/tompreston/dotfiles Documents/dotfiles
+	make -C Documents/dotfiles install
+}
 
-(cd ~/.vim/bundle;
-	git clone https://github.com/tpope/vim-sensible.git;
-	git clone https://github.com/tpope/vim-surround.git;
-	git clone https://github.com/bogado/file-line.git;
-	git clone https://github.com/embear/vim-localvimrc;
-	git clone https://github.com/kergoth/vim-bitbake)
+setup_vim()
+{
+	# vim pathogen and plugins
+	mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+		curl -LSso ~/.vim/autoload/pathogen.vim \
+			https://tpo.pe/pathogen.vim
+
+	(cd ~/.vim/bundle;
+		git clone https://github.com/tpope/vim-sensible.git;
+		git clone https://github.com/tpope/vim-surround.git;
+		git clone https://github.com/bogado/file-line.git;
+		git clone https://github.com/embear/vim-localvimrc;
+		git clone https://github.com/kergoth/vim-bitbake)
+}
+
+check_keys
+apt_install
+gen_keys
+setup_git
+get_journal
+get_notes
+get_dotfiles
+setup_vim
