29302b3ca7ae38bfb525a379b5c3d0d9564c83bf
Thomas Preston <thomas.preston@codethink.co.uk>
2019-07-01 12:39:21 +0100

    setup: git: Add alias for diff

--- setup/git.sh
+++ setup/git.sh
@@ -5,4 +5,5 @@ git config --global user.name "Thomas Preston"
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
+git config --global alias.di diff
 git config --global alias.st status
