9fb69f9a5be96287455b9c6db08f79042625b8b7
Tom Preston <tompreston@monzo.com>
2022-03-27 15:25:01 +0100

    nvim: Remove the snippets lines because I don't use them

--- home/.config/nvim/lua/lsp_config.lua
+++ home/.config/nvim/lua/lsp_config.lua
@@ -3,9 +3,6 @@
 -- * https://www.getman.io/posts/programming-go-in-neovim/
 -- * https://github.com/neovim/nvim-lspconfig
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-
 local on_attach = function(client, bufnr)
 	local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
 	local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
@@ -58,11 +55,8 @@ end
 
 require('lspconfig').gopls.setup{
 	cmd = {'gopls'},
-	-- for postfix snippets and analyzers
-	capabilities = capabilities,
 	settings = {
 		gopls = {
-			experimentalPostfixCompletions = true,
 			analyses = {
 				unusedparams = true,
 				shadow = true,
