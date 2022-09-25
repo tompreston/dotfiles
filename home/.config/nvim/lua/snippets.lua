0333ce883eab05243b4175bfc0828213162741a9
Tom Preston <tompreston@monzo.com>
2022-09-25 19:33:26 +0100

    nvim: luasnip: update more often

--- home/.config/nvim/lua/snippets.lua
+++ home/.config/nvim/lua/snippets.lua
@@ -3,6 +3,12 @@ require("luasnip.loaders.from_vscode").lazy_load({
 	paths = { "./friendly-snippets" }
 })
 
+local luasnip = require("luasnip")
+luasnip.setup({
+	-- Update more often, :h events for more info.
+	update_events = "TextChanged,TextChangedI",
+})
+
 -- press <Tab> to expand or jump in a snippet. These can also be mapped separately
 -- via <Plug>luasnip-expand-snippet and <Plug>luasnip-jump-next.
 vim.cmd("imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>'")
