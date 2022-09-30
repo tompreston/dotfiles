fd277626ec850f5190e309cb8f3eba99ff74f64e
Tom Preston <tompreston@monzo.com>
2022-09-30 23:34:34 +0100

    nvim: bit of tidying

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -41,39 +41,15 @@ vim.api.nvim_set_hl(0, 'Whitespace', {
 })
 
 -- 🌲 Treesitter
-require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all"
+require('nvim-treesitter.configs').setup({
   ensure_installed = { "c", "lua", "rust", "go", "bash", "python", "javascript" },
-
-  -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
-
-  -- Automatically install missing parsers when entering buffer
   auto_install = true,
-
-  -- List of parsers to ignore installing (for "all")
-  ignore_install = { "javascript" },
-
-  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
   highlight = {
-    -- `false` will disable the whole extension
     enable = true,
-
-    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-    -- the name of the parser)
-    -- list of language that will be disabled
-    --disable = { "c", "rust" },
-
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
   },
-}
+})
 
 -- 🐻 Fuzzy finder
 -- The vim.fzf interface is much faster than Telescope and this related issue
@@ -101,9 +77,7 @@ telescope.load_extension('fzf')
 require("luasnip.loaders.from_vscode").lazy_load({
 	paths = { "./friendly-snippets" }
 })
-
-local luasnip = require("luasnip")
-luasnip.setup({
+require("luasnip").setup({
 	-- Update more often, :h events for more info.
 	update_events = "TextChanged,TextChangedI",
 })
