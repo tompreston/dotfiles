8c0831482d9db0d4f7e0faf1501a5622e4d25980
Tom Preston <tompreston@monzo.com>
2022-09-30 22:32:20 +0100

    nvim: move snippets to main file

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -1,5 +1,4 @@
 require("lsp_config")
-require("snippets")
 
 -- ⚙️  Options
 vim.opt.background = "light"
@@ -13,7 +12,7 @@ vim.opt.list = true
 -- Highlight all matches in incsearch. C-l to clear
 vim.opt.hlsearch = true
 
--- 📽 Projectionist
+-- 🎥 Projectionist
 -- https://github.com/tpope/vim-projectionist
 -- help expand
 vim.g.projectionist_heuristics = {
@@ -77,11 +76,13 @@ require'nvim-treesitter.configs'.setup {
 }
 
 -- 🐻 Fuzzy finder
--- The vim.fzf interface is much faster than Telescope and the related issue was closed.
+-- The vim.fzf interface is much faster than Telescope and this related issue
+-- was closed.
 -- https://github.com/nvim-telescope/telescope.nvim/issues/1837#issuecomment-1113962736
 --
 -- Use the Telescope shortcut to launch it. If it gets fixed in telescope
--- switch back over. TODO look into this.
+-- switch back over.
+-- TODO look into this.
 vim.keymap.set('n', '<leader>ff', ':<C-u>FZF<CR>', { noremap = true })
 
 -- 🔭 Telescope
@@ -95,3 +96,24 @@ vim.keymap.set('n', '<leader>fh', telescopebuiltin.help_tags)
 local telescope = require('telescope')
 telescope.setup()
 telescope.load_extension('fzf')
+
+-- ✂️  Snippets
+require("luasnip.loaders.from_vscode").lazy_load({
+	paths = { "./friendly-snippets" }
+})
+
+local luasnip = require("luasnip")
+luasnip.setup({
+	-- Update more often, :h events for more info.
+	update_events = "TextChanged,TextChangedI",
+})
+
+-- press <Tab> to expand or jump in a snippet. These can also be mapped separately
+-- via <Plug>luasnip-expand-snippet and <Plug>luasnip-jump-next.
+vim.cmd("imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>'")
+vim.cmd("inoremap <silent> <S-Tab> <cmd>lua require'luasnip'.jump(-1)<Cr>")
+vim.cmd("snoremap <silent> <Tab> <cmd>lua require('luasnip').jump(1)<Cr>")
+vim.cmd("snoremap <silent> <S-Tab> <cmd>lua require('luasnip').jump(-1)<Cr>")
+-- For changing choices in choiceNodes (not strictly necessary for a basic setup).
+vim.cmd("imap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'")
+vim.cmd("smap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'")
