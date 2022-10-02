b2f17a5860d9602f5228b48c0a6bb9c9e8029d38
Tom Preston <tompreston@monzo.com>
2022-10-02 17:01:33 +0100

    nvim: Just use one file

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -1,5 +1,3 @@
-require("lsp_config")
-
 -- ⚙️  Options
 vim.opt.background = "light"
 vim.opt.colorcolumn = "101"
@@ -91,3 +89,105 @@ vim.cmd("snoremap <silent> <S-Tab> <cmd>lua require('luasnip').jump(-1)<Cr>")
 -- For changing choices in choiceNodes (not strictly necessary for a basic setup).
 vim.cmd("imap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'")
 vim.cmd("smap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'")
+
+-- 🔮 LSP config
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+local opts = { noremap=true, silent=true }
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+	-- Enable completion triggered by <c-x><c-o>
+	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+	
+	-- Mappings.
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	local bufopts = { noremap=true, silent=true, buffer=bufnr }
+	vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+	vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+	vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+	vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+	vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+	vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+	vim.keymap.set('n', '<space>wl', function()
+	  print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, bufopts)
+	vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+	vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+	vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+	vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+	vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+end
+
+require('lspconfig')['pyright'].setup{
+	on_attach = on_attach,
+}
+
+-- Rust, auto-format on save
+-- TODO use LSP for Rust
+-- https://rust-analyzer.github.io/manual.html#nvim-lsp
+-- let g:rustfmt_autosave = 1
+require('lspconfig')['rust_analyzer'].setup{
+	on_attach = on_attach,
+	-- Server-specific settings...
+	settings = {
+		["rust-analyzer"] = {}
+	}
+}
+
+-- Setup Go LSP client
+local util = require "lspconfig.util"
+require('lspconfig')['gopls'].setup{
+	cmd = {"gopls", "serve"},
+	filetypes = {"go", "gomod"},
+
+	-- Ignore typical project roots which cause gopls to ingest large monorepos.
+	--root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+	root_dir = util.root_pattern("main.go", "README.md", "LICENSE"),
+
+	settings = {
+		gopls = {
+			analyses = {
+				unusedparams = true,
+				shadow = true,
+			},
+			staticcheck = true,
+
+			-- Don't try to find the go.mod file, otherwise we ingest large monorepos
+			expandWorkspaceToModule = false,
+			memoryMode = "DegradeClosed",
+			directoryFilters = {
+				"-vendor",
+				"-manifests",
+			},
+		},
+	},
+
+	on_attach = on_attach,
+}
+
+-- LSP config for Go
+-- Organise imports like goimports
+-- https://github.com/golang/tools/blob/master/gopls/doc/vim.md#imports
+function go_org_imports(wait_ms)
+	local params = vim.lsp.util.make_range_params()
+	params.context = {only = {"source.organizeImports"}}
+	local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
+	for cid, res in pairs(result or {}) do
+		for _, r in pairs(res.result or {}) do
+			if r.edit then
+				local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+				vim.lsp.util.apply_workspace_edit(r.edit, enc)
+			end
+		end
+	end
+end
+
+-- https://www.getman.io/posts/programming-go-in-neovim/
+vim.cmd("autocmd BufWritePre *.go lua vim.lsp.buf.formatting_sync(nil, 1000)")
+vim.cmd("autocmd BufWritePre *.go lua go_org_imports()")
